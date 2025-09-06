a616d54d31edfc5e75202fa5f8496bfa461e1fa6
Akd3435 <141384274+Akd3435@users.noreply.github.com>
2025-09-07 00:21:04 +0530

    Refactor blockchain main logic and transaction handling

--- main.c
+++ main.c
@@ -1,46 +1,3 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "blockchain.h"
-
-#define MAX_TRANSACTIONS 100
-#define BLOCK_SIZE 1024
-
-int main() {
-    Blockchain bc;
-    init_blockchain(&bc);
-
-    int n;
-    printf("Enter number of transactions for block: ");
-    scanf("%d", &n);
-    getchar();
-
-    if (n <= 0 || n > MAX_TRANSACTIONS) {
-        printf("Invalid number of transactions.\n");
-        return 1;
-    }
-
-    char **transactions = (char **)malloc(sizeof(char *) * n);
-    for (int i = 0; i < n; i++) {
-        transactions[i] = (char *)malloc(BLOCK_SIZE);
-        printf("Transaction %d: ", i + 1);
-        scanf(" %[^\n]", transactions[i]);
-    }
-
-    add_block(&bc, transactions, n);
-    printf("\nBlock added to blockchain.\n");
-
-    print_blockchain(&bc);
-
-    for (int i = 0; i < n; i++)
-        free(transactions[i]);
-    free(transactions);
-
-    free_blockchain(&bc);
-    return 0;
-}
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -49,11 +6,11 @@ int main() {
 #define MAX_TRANSACTIONS 100
 #define BLOCK_SIZE 1024
 
-// Function to input transactions from user
+// Input transactions
 void input_transactions(char ***transactions, int *n) {
     printf("Enter number of transactions for block: ");
     scanf("%d", n);
-    getchar();  // consume newline
+    getchar();
 
     if (*n <= 0 || *n > MAX_TRANSACTIONS) {
         printf("Invalid number of transactions.\n");
@@ -62,59 +19,19 @@ void input_transactions(char ***transactions, int *n) {
     }
 
     *transactions = (char **)malloc(sizeof(char *) * (*n));
-    if (!*transactions) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
-
     for (int i = 0; i < *n; i++) {
         (*transactions)[i] = (char *)malloc(BLOCK_SIZE);
-        if (!(*transactions)[i]) {
-            perror("Memory allocation failed");
-            exit(EXIT_FAILURE);
-        }
         printf("Transaction %d: ", i + 1);
         scanf(" %[^\n]", (*transactions)[i]);
     }
 }
 
-// Function to free the dynamically allocated transactions array
+// Free transactions
 void free_transactions(char **transactions, int n) {
     if (!transactions) return;
     for (int i = 0; i < n; i++)
         free(transactions[i]);
     free(transactions);
-}
-
-// Function to tamper with a block and replace the first transaction
-void tamper_transaction(Blockchain *bc) {
-    int block_num;
-    printf("Enter block number to tamper: ");
-    scanf("%d", &block_num);
-
-    if (block_num < 0 || block_num >= (int)bc->size) {
-        printf("Invalid block number.\n");
-        return;
-    }
-
-    char buffer[BLOCK_SIZE];
-    printf("Enter new fake transaction to replace the first one:\n> ");
-    scanf(" %[^\n]", buffer);
-
-    // Replace first transaction
-    free(bc->blocks[block_num].m_tree.transactions[0]);
-    bc->blocks[block_num].m_tree.transactions[0] = strdup(buffer);
-
-    // Rebuild Merkle tree and update block hash
-    free_merkle_tree(&(bc->blocks[block_num].m_tree));
-    build_merkle_tree(bc->blocks[block_num].m_tree.transactions,
-                      bc->blocks[block_num].m_tree.num_transactions,
-                      &(bc->blocks[block_num].m_tree));
-
-    free(bc->blocks[block_num].block_hash);
-    bc->blocks[block_num].block_hash = compute_block_hash(&bc->blocks[block_num]);
-
-    printf("\nTampering complete. Block %d has been modified.\n", block_num);
 }
 
 int main() {
@@ -126,8 +43,7 @@ int main() {
         printf("\n====== BLOCKCHAIN MENU ======\n");
         printf("1. Add Block\n");
         printf("2. Print Blockchain\n");
-        printf("3. Tamper Transaction\n");
-        printf("4. Exit\n");
+        printf("3. Exit\n");
         printf("Choose an option: ");
         scanf("%d", &choice);
 
@@ -143,9 +59,6 @@ int main() {
             print_blockchain(&bc);
         }
         else if (choice == 3) {
-            tamper_transaction(&bc);
-        }
-        else if (choice == 4) {
             free_blockchain(&bc);
             printf("Exiting. Blockchain memory cleaned.\n");
             break;
